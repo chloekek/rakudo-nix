6946d1aa5d62ecbb18d697dd24ca89ba0c92cd51
Chloé <rightfold+ck@gmail.com>
2019-07-28 13:25:58 +0200

    Add profile template patch

--- nqp.nix
+++ nqp.nix
@@ -9,6 +9,7 @@ stdenv.mkDerivation rec {
         sha256 = "1v3i05ghk8i9y1ikjb6629hl5dg9f7hnkarc0vzpymlyij3077m5";
     };
     buildInputs = [moar perl];
+    patches = [./0001-profile-template.patch];
     configureScript = "perl ./Configure.pl";
     configureFlags = [
         "--backends=moar"
